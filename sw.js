25f678eb050b0b499002d32c6d34c43e1af79bfb
rsscavalcanti-cmyk <rsscavalcanti@gmail.com>
2025-09-20 03:56:47 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,43 +1,17 @@
-const BASE  = '/System-Engenharia/';   // prefixo do repositório no GitHub Pages
-const CACHE = 'system-painel-v1';
-
-const ASSETS = [
-  BASE,
-  BASE + 'index.html',
-  BASE + 'manifest.webmanifest',
-  BASE + 'icon-192.png',
-  BASE + 'icon-512.png',
-  BASE + 'assets/system-logo.jpg'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('system-painel-v1').then(cache => cache.addAll([
+      './',
+      './index.html',
+      './manifest.webmanifest',
+      './icon-192.png',
+      './icon-512.png'
+    ]))
   );
-  self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
-    ))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (event) => {
-  const req = event.request;
-  event.respondWith(
-    caches.match(req).then((cached) => {
-      if (cached) return cached;
-      return fetch(req).then((res) => {
-        if (req.method === 'GET' && new URL(req.url).origin === location.origin) {
-          const clone = res.clone();
-          caches.open(CACHE).then((cache) => cache.put(req, clone));
-        }
-        return res;
-      }).catch(() => caches.match(BASE + 'index.html'));
-    })
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
   );
 });
