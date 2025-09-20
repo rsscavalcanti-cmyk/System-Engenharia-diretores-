76e89d463098fd438815fba3e0cf86a9b6092922
rsscavalcanti-cmyk <rsscavalcanti@gmail.com>
2025-09-20 03:29:27 -0300

    update sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,30 @@
-const CACHE = 'painel-system-v1';
+const CACHE_NAME = 'system-painel-v1';
 const ASSETS = [
 './',
 './index.html',
 './manifest.webmanifest',
-'./sw.js',
-'./assets/system-logo.jpg'
+'./assets/system-logo.jpg',
+'./assets/icons/icon-192.png',
+'./assets/icons/icon-512.png'
 ];
 
 
 self.addEventListener('install', (event) => {
 event.waitUntil(
-caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
 );
 });
 
 
 self.addEventListener('activate', (event) => {
 event.waitUntil(
-caches.keys().then(keys => Promise.all(
-keys.filter(k => k !== CACHE).map(k => caches.delete(k))
-))
+caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k))))
 );
 });
 
 
 self.addEventListener('fetch', (event) => {
 event.respondWith(
-caches.match(event.request).then((cached) => cached || fetch(event.request))
+caches.match(event.request).then(resp => resp || fetch(event.request))
 );
 });
